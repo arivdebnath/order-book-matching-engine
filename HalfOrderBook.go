342b73c4d3511d11b404719d2ea34b5b0fcab4b0
Ariv Debnath <arivdebnath10@gmail.com>
2026-05-25 00:52:36 +0530

    adds order cancellation in halfBook

--- HalfOrderBook.go
+++ HalfOrderBook.go
@@ -3,7 +3,7 @@ package order_book
 import "fmt"
 
 type HalfOrderBook struct {
-	Side         Side //
+	Side         Side
 	Levels       []*PriceLevel
 	bestPriceIdx int // -1 during initialization or for empty Levels
 	MinPrice     uint64
@@ -49,6 +49,36 @@ func (hb *HalfOrderBook) Add(order *Order) error {
 		hb.bestPriceIdx = idx
 	} else if hb.Side == Sell && hb.bestPriceIdx > idx {
 		hb.bestPriceIdx = idx
+	}
+	return nil
+}
+
+func (hb *HalfOrderBook) CancelOrder(orderId uint64, price uint64) error {
+	if price < hb.MinPrice || price > hb.MaxPrice {
+		return fmt.Errorf("price %d is out of range", price)
+	}
+	idx := int(price - hb.MinPrice)
+	hb.Levels[idx].CancelOrder(orderId)
+	if hb.Levels[idx].TotalQty == 0 && hb.bestPriceIdx == idx {
+		if hb.Side == Sell {
+			for i := idx + 1; i < len(hb.Levels); i++ {
+				if hb.Levels[i] != nil {
+					hb.bestPriceIdx = i
+					break
+				}
+			}
+		} else {
+			for i := idx - 1; i >= 0; i-- {
+				if hb.Levels[i] != nil {
+					hb.bestPriceIdx = i
+					break
+				}
+			}
+		}
+		if hb.bestPriceIdx == idx {
+			hb.bestPriceIdx = -1
+		}
+		hb.Levels[idx] = nil
 	}
 	return nil
 }
